296fdb044c1fc7579a8f9f3c6e73821263e55ce1
valecristiano <valeriamdcristiano@gmail.com>
2026-03-18 18:57:11 +0300

    homepage links fix

--- components/MovieCard.jsx
+++ components/MovieCard.jsx
@@ -11,21 +11,6 @@ export default function MovieCard({ movie, label }) {
             <Link to={"/" + movie.id} className="p-1">
               <img src={movie.image} className="img-card" alt={movie.title} />
             </Link>
-
-            {/* <div className="card-body">
-              <h5 className="card-title">{movie.title}</h5>
-              <p className="card-text">{movie.abstract}</p>
-            </div>
-            <ul className="list-group list-group-flush">
-              <li className="list-group-item fw-bold list-group-item-secondary text-white">{movie.director}</li>
-              <li className="list-group-item list-group-item-secondary text-white">{movie.release_year}</li>
-              <li className="list-group-item list-group-item-secondary text-white">{movie.genre}</li>
-              <li className="list-group-item text-white list-group-item-secondary">
-                <Link to={"/" + movie.id} className="link-underline-secondary text-white">
-                  Details
-                </Link>
-              </li>
-            </ul> */}
           </div>
         </div>
       </div>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import MovieCard from "../../components/MovieCard";
 import { useEffect, useState } from "react";
+import { Link } from "react-router";
 
 const latestUrl = "http://localhost:3000/movies/latest";
 const bestRatedUrl = "http://localhost:3000/movies/bestrated";
@@ -23,13 +24,24 @@ export default function HomePage() {
 
   return (
     <>
-      <section className="container-fluid">
+      <section className="container">
         <div className="row row-cols-1 row-cols-md-2 g-5">
           <div className="col">
             <MovieCard movie={latestMovie} label="Latest Movie"></MovieCard>
           </div>
           <div className="col">
             <MovieCard movie={bestMovie} label="Best Rated Movie"></MovieCard>
+          </div>
+        </div>
+      </section>
+      <section className="container mt-4">
+        <div className=" row g-5">
+          <div className="col-12">
+            <div className="card h-100 w-100 bg-secondary-subtle border border-warning-subtle text-center">
+              <Link to={"/movies"} className="card-body p-1 text-center text-decoration-none">
+                <h2 className="card-title ">Movies List</h2>
+              </Link>
+            </div>
           </div>
         </div>
       </section>
